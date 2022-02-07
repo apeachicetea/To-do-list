a0ef1f1fe6c176f288473d94d153dfaaaa675a02
kimdaeyoon <eodbszla@gmail.com>
2022-02-07 14:18:18 +0900

    Setting

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+  accentColor: "#9c88ff",
+  cardBgColor: "white",
+};
